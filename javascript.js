bf3cf42b157a0cc625ab462cdea2b173c7019cca
DaveKnud <daveknud@outlook.com>
2023-04-27 16:33:26 +0200

    creando qualcosina

---
+++ javascript.js
@@ -0,0 +1,26 @@
+//General variables
+let numberKm,agePassenger,normalPrice;
+//Constant
+const pricePerKm = 0.21;
+
+//Calculate Normal Price
+normalPrice = numberKm * pricePerKm;
+
+//Ask Number Km
+numberKm = prompt("How many km do you wanna do?");
+
+
+// Ask Age Passenger
+agePassenger = prompt("How old are you?");
+
+
+
+//Discount minors 20%
+
+
+//Discount Over 65
+
+//Output
+
+
+
