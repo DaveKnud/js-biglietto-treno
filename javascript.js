957c058acfff71f911d34cf0e9648c67cd861512
DaveKnud <daveknud@outlook.com>
2023-04-27 17:00:50 +0200

    creando sconto minorenni

--- javascript.js
+++ javascript.js
@@ -1,10 +1,8 @@
 //General variables
-let numberKm,agePassenger,normalPrice;
+let numberKm,agePassenger,normalPrice,minorsPrice,over65Price,finalPrice;
 //Constant
 const pricePerKm = 0.21;
 
-//Calculate Normal Price
-normalPrice = numberKm * pricePerKm;
 
 //Ask Number Km
 numberKm = prompt("How many km do you wanna do?");
@@ -15,12 +13,27 @@ console.log(numberKm)
 agePassenger = prompt("How old are you?");
 console.log(agePassenger);
 
+//Calculate Normal Price
+normalPrice = numberKm * pricePerKm;
+console.log(normalPrice)
 
+
+//CONDITIONS
 
 //Discount minors 20%
+if (agePassenger < 18
+)
+{
+minorsPrice = normalPrice - ((20 / 100) * normalPrice);
+console.log(minorsPrice);}
+
+else {
+    finalPrice = normalPrice;
+}
 
 
-//Discount Over 65
+//Discount Over 65 40%
+
 
 //Output
 
